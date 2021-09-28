2c3609ba7c0841bee17221257a42716a138ab70e
Nazim <nazimali@gmail.com>
2021-09-28 00:03:52 -0400

    UI: Fix thumbnail responsiveness

--- src/components/gallery/GalleryList.tsx
+++ src/components/gallery/GalleryList.tsx
@@ -11,11 +11,11 @@ function GalleryList({ loading, users }) {
   return (
     <Grid
       container
-      spacing={{ xs: 2, md: 4 }}
-      columns={{ xs: 2, sm: 4, md: 6 }}
+      spacing={{ xs: 2, md: 3 }}
+      columns={{ xs: 2, sm: 4, md: 6, lg: 8 }}
     >
       {Array.from(loading ? Array(20) : users).map((user, index) => (
-        <Grid item key={index} sx={{ width: 200, height: 300 }} >
+        <Grid item xs={2} sm={2} md={2} lg={2} key={index}>
           <GalleryListItem key={index}>
             {loading ? <LoadingSkeleton index={index} /> : <User user={user} />}
           </GalleryListItem>
